f139f3476e1b8d7c083f4892c59ce5a010c987dd
hari6091 <hari.junior19@gmail.com>
2022-11-10 21:43:21 -0300

    fix: fixes CORS error

--- src/hooks/useBackend.tsx
+++ src/hooks/useBackend.tsx
@@ -2,14 +2,22 @@ import { useEffect, useState } from 'react';
 
 import api from '../services/api';
 
+type User = {
+  created_at: string;
+  email: string;
+  id: string;
+  name: string;
+  password: string;
+};
+
 const useBackend = async () => {
-  const [data, setData] = useState();
+  const [data, setData] = useState<User[]>();
 
   useEffect(() => {
     api.get('users').then((response) => setData(response.data));
   }, []);
 
-  return { data };
+  return data;
 };
 
 export default useBackend;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001',
+  baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
