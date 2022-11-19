2bdfa5f2473bc091fca27e3c1eb35acac6f9b0c1
hari6091 <hari.junior19@gmail.com>
2022-11-19 19:19:16 -0300

    chore: uncomment protected layout

--- src/components/ProtectedLayout/index.tsx
+++ src/components/ProtectedLayout/index.tsx
@@ -5,8 +5,8 @@ import { useAuth } from '../../hooks/useAuth';
 export function ProtectedLayout({ children }: { children: JSX.Element }) {
   const auth = useAuth();
 
-  // if (!auth.email) {
-  //   return <h1>Faça login para continuar</h1>;
-  // }
+  if (!auth.email) {
+    return <h1>Faça login para continuar</h1>;
+  }
   return children;
 }
