5dba113829903f7dba096db4bebe4e073fbf75bb
hari6091 <hari.junior19@gmail.com>
2022-11-20 14:54:41 -0300

    style: improve signin page styles

--- src/packages/pages/Signin/Signin.tsx
+++ src/packages/pages/Signin/Signin.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
-import { Alert } from '@mui/material';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Alert, Box, Button, Grid, TextField } from '@mui/material';
 
 import { useAuth } from '../../../hooks/useAuth';
-import { Button } from '../../ui-kit/Button';
-import Input from '../../ui-kit/Input/Input';
 import * as C from './styles';
 
 function Signin() {
@@ -32,37 +31,85 @@ function Signin() {
   };
 
   return (
-    <C.Container>
-      <C.Label>SISTEMA DE LOGIN</C.Label>
-      <C.Content>
-        <Input
-          type="email"
-          placeholder="Digite seu E-mail"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-            setError('');
-          }}
-        />
-        <Input
-          type="password"
-          placeholder="Digite sua Senha"
-          value={senha}
-          onChange={(e) => {
-            setSenha(e.target.value);
-            setError('');
-          }}
-        />
-        <C.labelError>{error}</C.labelError>
-        <Button onClick={handleLogin}>Entrar</Button>
-        <C.LabelSignup>
-          Não tem uma conta?
-          <C.Strong>
-            <Link to="/signup">&nbsp;Registre-se</Link>
-          </C.Strong>
-        </C.LabelSignup>
-      </C.Content>
-    </C.Container>
+    <Grid
+      container
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center"
+    >
+      <Grid
+        container
+        item
+        xs={12}
+        sm={5}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+        bgcolor="#d3d3d3"
+      >
+        <Box maxWidth="400px">
+          <C.Title>BEM-VINDO A SEMADEC</C.Title>
+          <C.Subtitle>DATA_DO_EVENTO</C.Subtitle>
+          <Button
+            variant="contained"
+            onClick={() => {
+              navigate('/signup');
+            }}
+          >
+            Cadastrar-se
+          </Button>
+        </Box>
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={7}
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        minWidth="400px"
+      >
+        <Box
+          p="20px"
+          gap="15px"
+          borderRadius="5px"
+          boxShadow="0 1px 2px #0003"
+          maxWidth="350px"
+        >
+          <AccountCircleIcon style={{ fontSize: 150 }} />
+
+          <TextField
+            label="E-mail escolar"
+            variant="outlined"
+            fullWidth
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <TextField
+            type="password"
+            label="Digite sua Senha"
+            variant="outlined"
+            fullWidth
+            value={senha}
+            margin="normal"
+            onChange={(e) => {
+              setSenha(e.target.value);
+            }}
+          />
+          <C.labelError>{error}</C.labelError>
+          <Box textAlign="end" width="100%">
+            <Button variant="contained" onClick={handleLogin}>
+              Entrar
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
